883f6583d45b1c43603dd52f1e2009425e03dc8e
sidd1811 <s.siddharth.svnit@gmail.com>
2018-01-19 02:18:14 +0530

    Commit#2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var router = express.Router();
 var path = __dirname + '/app/';
@@ -20,6 +21,27 @@ router.get("/website/employer/dashboard.html",function(req,res){
 router.get("/website/applicant/dashboard.html",function(req,res){
   res.sendFile(path + "website/applicant/dashboard.html");
 });
+
+app.use(express.static('/'));
+app.use(express.static('app'));
+app.use(express.static('lib'));
+app.use(express.static('website'));
+
+//Method to post job parameters to jobpage
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.post("/website/employer/addnewjob.html", function(req,res){
+  console.log(req.body);
+  //update eth
+});
+
+// app.get("/website/employer/applicantlist.html/:jobArray",function(req,res,next){
+//   // res.
+// }
 
 app.use("/", router);
 
